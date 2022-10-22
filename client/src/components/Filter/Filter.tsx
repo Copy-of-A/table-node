69fd58ade17ea37ef1cffe7da3100b9866eb3c2d
Alla <alla@redcollar.ru>
2022-10-22 16:10:37 +0300

    add a little bit styling

--- client/src/components/Filter/Filter.tsx
+++ client/src/components/Filter/Filter.tsx
@@ -1,13 +1,9 @@
-import { FC } from "react";
 import { FilterColumnList, FilterQueryList } from "../const";
 import { DropDown } from "../DropDown/DropDown";
 import { useFilter } from "./filter.hook";
+import styles from './filter.module.scss';
 
-export interface FilterProps {
-
-}
-
-export const Filter: FC<FilterProps> = () => {
+export const Filter = () => {
     const {
         handleValueChange,
         handleColumnChange,
@@ -19,10 +15,13 @@ export const Filter: FC<FilterProps> = () => {
     } = useFilter()
     return (
         <>
+            <p>Выберете колонку: </p>
             <DropDown options={FilterColumnList} value={column} selectId={"FilterColumn"} handleChange={handleColumnChange} />
+            <p>Выберете условие: </p>
             <DropDown options={FilterQueryList} value={query} selectId={"FilterColumn"} handleChange={handleQueryChange} />
-            <input value={inputValue} onChange={handleValueChange}></input>
-            <button onClick={handleResetClick}>Reset filter</button>
+            <p>Введите значение: </p>
+            <input className={styles.input} value={inputValue} onChange={handleValueChange}></input>
+            <button className={styles.button} onClick={handleResetClick}>Сбросить фильтр</button>
         </>
     )
 }
--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -2,6 +2,7 @@ import { FC } from "react"
 import { Filter } from "../Filter/Filter";
 import { Row } from "../Row/Row";
 import { TableProps, useTableHook } from "./table.hook"
+import styles from './table.module.scss'
 
 export const Table: FC<TableProps> = ({ filterQuery }) => {
     console.log("filter", filterQuery)
@@ -13,7 +14,7 @@ export const Table: FC<TableProps> = ({ filterQuery }) => {
     return (
         <div>
             <Filter />
-            <table>
+            <table className={styles.table}>
                 <thead>
                     <tr>
                         <th>Title</th>
