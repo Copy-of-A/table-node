3482c6f385ef4d864ea77269952ca745c78412a5
Alla <alla@redcollar.ru>
2022-10-22 17:36:59 +0300

    build for client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import  { App } from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter basename={import.meta.env.VITE_APP_BASE_PATH as string}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
